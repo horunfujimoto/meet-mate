d06f9640812319a7800e4cd54f3da2f6f925f852
horun.fujimoto <hello.horurun@gmail.com>
2024-03-28 01:22:37 +0000

    [ADD]match_usersテーブル作成

---
+++ database/migrations/2024_03_28_101914_create_match_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('match_users', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('name', 20);
+            $table->string('address', 20);
+            $table->string('work', 20);
+            $table->string('birthday');
+            $table->string('sns');
+            $table->string('way', 20);
+            $table->string('others');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('match_users');
+    }
+};
