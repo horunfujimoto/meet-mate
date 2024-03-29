b74fe956f73f637e3f2fe7d0d7e0a37f3dc6ee0e
horun.fujimoto <hello.horurun@gmail.com>
2024-03-29 02:56:46 +0000

    [ADD]ヘッダーレイアウト修正

--- resources/views/commons/link_items.blade.php
+++ resources/views/commons/link_items.blade.php
@@ -1,10 +1,10 @@
 @if (Auth::check())
     {{-- ログアウトへのリンク --}}
-    <li><a class="link link-hover" href="#" onclick="event.preventDefault();this.closest('form').submit();">ログアウト</a></li>
-    <li><a class="link link-hover" href="{{ route('dashboard') }}">リンク！！いまだけおいておく</a>
+    <li class="list-inline-item"><a class="link link-hover" href="#" onclick="event.preventDefault();this.closest('form').submit();">ログアウト</a></li>
+    <li class="list-inline-item"><a class="link link-hover" href="{{ route('dashboard') }}">リンク！！いまだけおいておく</a></li>
 @else
     {{-- ユーザ登録ページへのリンク --}}
-    <li><a class="link link-hover" href="{{ route('register') }}">サインイン</a></li>
+    <li class="list-inline-item"><a class="link link-hover" href="{{ route('register') }}">サインイン</a></li>
     {{-- ログインページへのリンク --}}
-    <li><a class="link link-hover" href="{{ route('login') }}">ログイン</a></li>
+    <li class="list-inline-item"><a class="link link-hover" href="{{ route('login') }}">ログイン</a></li>
 @endif
--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,17 +1,16 @@
 <header class="mb-4">
-    <nav class="navbar bg-neutral text-neutral-content">
+    <nav class="navbar navbar-dark bg-dark">
         {{-- トップページへのリンク --}}
-        <div class="flex-1">
-            <h1><a class="btn btn-ghost normal-case text-xl" href="/">meet-mate</a></h1>
+        <div class="d-flex align-items-center">
+            <h1 class="m-0 font-weight-bold"><a class="btn btn-link text-white" href="/">meet-mate</a></h1>
         </div>
         {{-- その他リンク --}}
-        <div class="flex-none">
+        <div class="d-flex align-items-center">
             <form method="POST" action="{{ route('logout') }}">
                 @csrf
-                <ul>
+                <ul class="list-inline mb-0 text-white d-flex">
                     @include('commons.link_items')
                 </ul>
-                </div>
             </form>
         </div>
     </nav>
