f0d3d6466f8626a5cc6c2dc10cdc308830bc6df8
horun.fujimoto <hello.horurun@gmail.com>
2024-03-29 05:37:26 +0000

    [FIX]topレイアウト微調整

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="prose hero bg-base-200 mx-auto max-w-full rounded">
-        <div class="hero-content text-center my-10">
-            <div class="max-w-md mb-10">
-                <h2>Welcome to the meet♥mate</h2>
-                {{-- ユーザ登録ページへのリンク --}}
-                <a class="btn btn-primary btn-lg normal-case" href="{{ route('register') }}">サインイン！</a>
+    <div class="flex justify-center items-center h-screen">
+        <div class="bg-white rounded p-6">
+            <div class="text-center my-10">
+                <div class="max-w-md mb-10">
+                    <h2 class="text-4xl text-black mb-8">Welcome to the meet♥mate</h2>
+                    {{-- ユーザ登録ページへのリンク --}}
+                    <a class="btn btn-primary btn-lg normal-case mt-4" href="{{ route('register') }}" style="margin-bottom: 20px;">サインイン！</a>
+                </div>
             </div>
         </div>
     </div>
