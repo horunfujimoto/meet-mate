0a4d5f4b62599a3a7a6927cfd799aa6f7d97966f
horun.fujimoto <hello.horurun@gmail.com>
2024-03-29 08:03:48 +0000

    [ADD]友達一覧ページ修正

--- resources/views/friends/index.blade.php
+++ resources/views/friends/index.blade.php
@@ -6,36 +6,14 @@
         </aside>
         <div class="sm:col-span-2 mt-4">
             <div class="mt-4">
-                <ul class="list-none">
-                @foreach ($users as $myfriend) {{-- $myfriends -> $users --}}
-                    <li class="flex items-center gap-x-2 mb-4">
-                        {{-- ユーザのメールアドレスをもとにGravatarを取得して表示 --}}
-                        <div class="avatar">
-                            <div class="w-12 rounded">
-                                <img src="{{ Gravatar::get($myfriend->email) }}" alt="" />
-                            </div>
-                        </div>
-                        <div>
-                            <div>
-                                {{ $myfriend->name }}
-                            </div>
-                            <div>
-                                {{-- 友達申請・解除ボタン --}}
-                                @include('friends.friend_button')
-                            </div>
-                        </div>
-                    </li>
-                @endforeach
-                </ul>
+                {{-- ユーザ一覧 --}}
+                @include('users.users')
+            </div>
 
-            {{-- 友達がいない場合のメッセージ --}}
+            {{-- 自分が申請中のユーザーがいない場合のメッセージ --}}
             @if($users->isEmpty())
-                <p>友達はまだいません。ユーザー一覧より友達申請をしてください。</p>
+                <p>友達申請中のユーザーはいません。</p>
             @endif
-            
-            {{-- ページネーションのリンク --}}
-            {{ $users->links() }}
-        　　</div>
-    　　</div>
+        </div>
     </div>
 @endsection
