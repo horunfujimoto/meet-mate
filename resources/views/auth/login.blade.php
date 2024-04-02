d618f97d00cc66afd4f1122ef9a060f71d9e11f4
horun.fujimoto <hello.horurun@gmail.com>
2024-04-02 04:08:28 +0000

    [UPDATE]レイアウト調整(ログイン・サインインページ)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Log in') }}</div>
+                <div class="card-header" style="background-color: #FFCCCC;">{{ __('ログイン') }}</div>
 
                 <div class="card-body">
                     <x-auth-session-status class="mb-4" :status="session('status')" />
@@ -15,14 +15,14 @@
 
                         <!-- Email Address -->
                         <div class="mb-3">
-                            <label for="email" class="form-label">{{ __('Email') }}</label>
+                            <label for="email" class="form-label">{{ __('メールアドレス') }}</label>
                             <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" :value="old('email')" required autofocus autocomplete="username">
                             <x-input-error :messages="$errors->get('email')" class="mt-2" />
                         </div>
 
                         <!-- Password -->
                         <div class="mb-3">
-                            <label for="password" class="form-label">{{ __('Password') }}</label>
+                            <label for="password" class="form-label">{{ __('パスワード') }}</label>
                             <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                             <x-input-error :messages="$errors->get('password')" class="mt-2" />
                         </div>
@@ -30,17 +30,17 @@
                         <!-- Remember Me -->
                         <div class="form-check mb-3">
                             <input class="form-check-input" type="checkbox" id="remember_me" name="remember">
-                            <label class="form-check-label" for="remember_me">{{ __('Remember me') }}</label>
+                            <label class="form-check-label" for="remember_me">{{ __('パスワードを記憶する') }}</label>
                         </div>
 
                         <div class="d-flex justify-content-end">
                             @if (Route::has('password.request'))
                                 <a class="btn btn-link text-decoration-none me-3" href="{{ route('password.request') }}">
-                                    {{ __('Forgot your password?') }}
+                                    {{ __('パスワードを忘れてしまった方はこちら') }}
                                 </a>
                             @endif
 
-                            <button type="submit" class="btn btn-primary">{{ __('Log in') }}</button>
+                            <button type="submit" class="btn" style=" background-color: #FF6699; color: white; font-size: 1.2rem;">{{ __('ログイン') }}</button>
                         </div>
                     </form>
                 </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header" style="background-color: #FFCCCC;">{{ __('サインイン') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
@@ -13,7 +13,7 @@
 
                         <!-- Name -->
                         <div class="mb-3">
-                            <label for="name" class="form-label">{{ __('Name') }}</label>
+                            <label for="name" class="form-label">{{ __('ニックネーム') }}</label>
                             <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
                             @error('name')
                                 <span class="invalid-feedback" role="alert">
@@ -24,7 +24,7 @@
 
                         <!-- Email Address -->
                         <div class="mb-3">
-                            <label for="email" class="form-label">{{ __('Email') }}</label>
+                            <label for="email" class="form-label">{{ __('メールアドレス') }}</label>
                             <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
                             @error('email')
                                 <span class="invalid-feedback" role="alert">
@@ -35,7 +35,7 @@
 
                         <!-- Password -->
                         <div class="mb-3">
-                            <label for="password" class="form-label">{{ __('Password') }}</label>
+                            <label for="password" class="form-label">{{ __('パスワード') }}</label>
                             <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
                             @error('password')
                                 <span class="invalid-feedback" role="alert">
@@ -46,13 +46,13 @@
 
                         <!-- Confirm Password -->
                         <div class="mb-3">
-                            <label for="password_confirmation" class="form-label">{{ __('Confirm Password') }}</label>
+                            <label for="password_confirmation" class="form-label">{{ __('パスワード(確認)') }}</label>
                             <input id="password_confirmation" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                         </div>
 
                         <div class="d-flex justify-content-between align-items-center mb-3">
-                            <a href="{{ route('login') }}" class="text-decoration-none">{{ __('Already registered?') }}</a>
-                            <button type="submit" class="btn btn-primary">{{ __('Register') }}</button>
+                            <a href="{{ route('login') }}" class="text-decoration-none">{{ __('すでに登録済みの方はこちら') }}</a>
+                            <button type="submit" class="btn" style=" background-color: #FF6699; color: white; font-size: 1.2rem;">{{ __('サインイン') }}</button>
                         </div>
                     </form>
                 </div>
