6c48b7eb4231684fbd68a11c9f30bc9b95da67a9
horun.fujimoto <hello.horurun@gmail.com>
2024-04-05 02:54:44 +0000

    [UPDATE]seederに初期user追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Way;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -31,6 +32,20 @@ class DatabaseSeeder extends Seeder
     
         // シーディングされたデータをデータベースに挿入
         Way::insert($ways);
-    
+        
+        $users = [
+            ['name' => 'ねこ', 'email'=> '000@co.jp','password'=> bcrypt('password')],
+            ['name' => 'たぬき', 'email'=> '111@co.jp','password'=> bcrypt('password')],
+            ['name' => 'いぬ', 'email'=> '222@co.jp','password'=> bcrypt('password')],
+            ['name' => 'ねずみ', 'email'=> '333@co.jp','password'=> bcrypt('password')],
+            ['name' => 'うさぎ', 'email'=> '444@co.jp','password'=> bcrypt('password')],
+            ['name' => 'りす', 'email'=> '555@co.jp','password'=> bcrypt('password')],
+            ['name' => 'へび', 'email'=> '666@co.jp','password'=> bcrypt('password')],
+            ['name' => 'たつ', 'email'=> '777@co.jp','password'=> bcrypt('password')],
+            ['name' => 'いのしし', 'email'=> '888@co.jp','password'=> bcrypt('password')],
+        ];
+        
+        // シーディングされたデータをデータベースに挿入
+        User::insert($users);
     }
 }
