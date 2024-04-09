20520e5484650f995ae936a07e5b36bbdb9fe7b3
horun.fujimoto <hello.horurun@gmail.com>
2024-04-09 06:29:26 +0000

    [ADD]コメントにrequired追加

--- resources/views/comments/form.blade.php
+++ resources/views/comments/form.blade.php
@@ -11,7 +11,7 @@
             @csrf
         
             <div class="form-control mt-4" style="display: flex; justify-content: space-between;">
-                <textarea rows="2" name="comment" class="input input-bordered w-full"></textarea>
+                <textarea rows="2" name="comment" class="input input-bordered w-full" required></textarea>
                 <button type="submit" class="btn normal-case" style="background-color: #FF6699; color: white; font-size: 1.2rem;"><i class="fa-regular fa-paper-plane"></i></button>
             </div>
         </form>
