05a2615d5c5a29979114a09538095670659a4402
horun.fujimoto <hello.horurun@gmail.com>
2024-04-09 01:45:04 +0000

    [FIX]Usersの変数の命名規則統一

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -12,10 +12,10 @@ class UsersController extends Controller
         if (\Auth::check()) {
         
             // ログインユーザーのIDを取得
-            $loggedInUserId = auth()->id();
+            $login_user_id = auth()->id();
             
             // ユーザークエリを取得
-            $query = User::where('id', '!=', $loggedInUserId)->orderBy('id', 'desc');
+            $query = User::where('id', '!=', $login_user_id)->orderBy('id', 'desc');
             
             // キーワードが指定されていれば、名前で部分一致検索を行う
             if ($request->has('keyword') && !empty($request->keyword)) {
@@ -60,12 +60,12 @@ class UsersController extends Controller
         $user = User::findOrFail($id);
     
         // 自分が友達申請を受けたユーザーかつ友達でないユーザー一覧を取得
-        $friendRequests = $user->friendRequests()->whereNotIn('users.id', $user->myfriends()->pluck('users.id'))->paginate(10);
+        $friend_requests = $user->friendRequests()->whereNotIn('users.id', $user->myfriends()->pluck('users.id'))->paginate(10);
     
         // 一覧ビューでそれらを表示
         return view('users.friendRequests', [
             'user' => $user,
-            'users' => $friendRequests,
+            'users' => $friend_requests,
         ]);
     }
 
