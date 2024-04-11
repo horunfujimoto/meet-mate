c8eeecf71adaff05e589dbe5afe9b1989d6a954c
horun.fujimoto <hello.horurun@gmail.com>
2024-04-11 08:31:15 +0000

    [UPDATE]投稿一覧レイアウト調整

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -12,14 +12,14 @@
             <div class="py-2">
                 @include('posts.nabtabs')
             </div>
-
-            <div class="pt-4">
-                {{-- 検索機能 --}}
-                <form action="{{ route('posts.index') }}" method="GET" class="ml-auto d-flex align-items-center">
-                    <input type="text" name="keyword" value="{{ $keyword }}" placeholder="タイトルのキーワードを入力" class="form-control mr-2" style="width: 15em;">
-                    <button type="submit" class="btn btn-hover" style="background-color: #FF6699; color: white; font-size: 1.2rem; margin-top: auto;"><i class="fa-solid fa-magnifying-glass"></i></button>
-                </form>
+            
+            {{-- 検索機能 --}}
+            <form action="{{ route('posts.index') }}" method="GET" class="ml-auto d-flex align-items-center">
+                <input type="text" name="keyword" value="{{ $keyword }}" placeholder="タイトルのキーワードを入力" class="form-control mr-2" style="width: 15em;">
+                <button type="submit" class="btn btn-hover" style="background-color: #FF6699; color: white; font-size: 1.2rem; margin-top: auto;"><i class="fa-solid fa-magnifying-glass"></i></button>
+            </form>
                 
+            <div class="pt-4">
                 {{-- 投稿一覧 --}}
                 @include('posts.posts')
                 
